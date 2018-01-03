0c7d0de5e4599ae829158b455cdc702ac3377408
cem dırman <cemdrman@gmail.com>
2018-01-03 23:36:11 +0300

    found most repated word

--- app/src/main/java/com/example/cemdirman/comkonusarakogren/activity/NewsDetailActivity.java
+++ app/src/main/java/com/example/cemdirman/comkonusarakogren/activity/NewsDetailActivity.java
@@ -10,6 +10,14 @@ import com.example.cemdirman.comkonusarakogren.R;
 import com.example.cemdirman.comkonusarakogren.model.News;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by cemdirman on 3.01.2018.
  */
@@ -53,11 +61,66 @@ public class NewsDetailActivity extends Activity {
                 .load(detailNews.getUrlToImage())
                 .fit()
                 .into(imgNewsDetailImage);
+
+        findRepatedWord(getListOfWord());
     }
 
-    private void findMostRecentWord(){
-        
+    private String[] getListOfWord(){
+        String words = txtNewsDetailDescription.getText().toString().concat(txtNewsDetailTitle.getText().toString());
+        String[] wordList = words.toLowerCase().split("[.? ,]");
+        return wordList;
     }
+
+    private void findRepatedWord(String[] wordList){
+        HashMap<String,Integer> counterMap = new HashMap<>();
+        int defaultValue = 1;
+        boolean isEmpty = true;
+        for (int i = 0; i < wordList.length; i++) {
+            if (isEmpty) {
+                counterMap.put(wordList[i], defaultValue); //just only first one
+                isEmpty = false;
+            }else{
+                if (counterMap.containsKey(wordList[i])) {
+                    counterMap.put(wordList[i],counterMap.get(wordList[i]) + 1);
+                }else{
+                    counterMap.put(wordList[i],defaultValue);
+                }
+            }
+        }
+
+        String[] sortedWordList = sortWordNumber(counterMap);
+        for (String s : sortedWordList) {
+            System.out.println(s);
+        }
+
+    }
+
+    private String[] sortWordNumber( HashMap<String,Integer> counterMap){
+        Set<Map.Entry<String, Integer>> entries = counterMap.entrySet();
+        Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String,Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                Integer v1 = e1.getValue();
+                Integer v2 = e2.getValue();
+                return v1.compareTo(v2);
+            }
+        };
+
+        List<Map.Entry<String, Integer>> listOfEntries = new ArrayList<>(entries);
+        Collections.sort(listOfEntries, valueComparator);
+        System.out.println("sorted: " + listOfEntries);
+
+        String[] sortedWords = new String[5];
+        int counter = 0;
+        for (int i = listOfEntries.size()-1; i > 0 ; i--) {
+            if (counter < 5) {
+                sortedWords[counter] = listOfEntries.get(i).getKey();
+                counter++;
+            }
+        }
+        return sortedWords;
+    }
+
 
     private void translate(){
 
