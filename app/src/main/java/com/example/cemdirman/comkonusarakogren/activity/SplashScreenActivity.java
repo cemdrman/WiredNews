f3365f31bd1a210f99dc7af11829fbfdaafcb83c
cem dırman <cemdrman@gmail.com>
2018-01-04 14:57:48 +0300

    added splash screen

---
+++ app/src/main/java/com/example/cemdirman/comkonusarakogren/activity/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.example.cemdirman.comkonusarakogren.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.cemdirman.comkonusarakogren.R;
+
+
+/**
+ * Created by cemdirman on 4.01.2018.
+ */
+
+public class SplashScreenActivity extends Activity{
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                //DB dolu ise main activity açılır boş ise login activity
+
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
